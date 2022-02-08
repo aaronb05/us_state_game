f75d6efefc78ad2672656a7a059a024f79715b4d
aaronb05 <aboyles05@gmail.com>
2022-02-08 11:49:49 -0500

    indent error

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ while playing:
     else:
         user_guess = screen.textinput(f"{new_state.correct_answers}/50 States Correct",
                                       "Please try and guess a state").title()
-    if user_guess in states:
-        user_guesses.append(user_guess)
-        state = data[data.state == user_guess]
-        x_cor = int(state.x)
-        y_cor = int(state.y)
-        print(f"{x_cor}, {y_cor}")
-        new_state.move_to_location(u_input=user_guess, x=x_cor, y=y_cor)
-    elif user_guess == "Exit":
-        states_to_learn = [state for state in states if state not in user_guesses]
-        # for i in states:
-        #     if i not in user_guesses:
-        #         missing_states.append(i)
-        #     else:
-        #         pass
-        new_data = pandas.DataFrame(states_to_learn, columns=["States"])
-        new_data.to_csv("states_to_learn2.csv")
-        break
+        if user_guess in states:
+            user_guesses.append(user_guess)
+            state = data[data.state == user_guess]
+            x_cor = int(state.x)
+            y_cor = int(state.y)
+            print(f"{x_cor}, {y_cor}")
+            new_state.move_to_location(u_input=user_guess, x=x_cor, y=y_cor)
+        elif user_guess == "Exit":
+            states_to_learn = [state for state in states if state not in user_guesses]
+            # for i in states:
+            #     if i not in user_guesses:
+            #         missing_states.append(i)
+            #     else:
+            #         pass
+            new_data = pandas.DataFrame(states_to_learn, columns=["States"])
+            new_data.to_csv("states_to_learn2.csv")
+            break
 
 screen.exitonclick()
 
